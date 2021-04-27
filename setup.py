68b5b5f3087cad297b1b2235c55004159e03b6ff
snkas <snkasdev@gmail.com>
2021-04-27 21:23:33 +0200

    v1.0.2

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='discrevpy',
-    version='1.0.1',
+    version='1.0.2',
     description='Minimalist discrete event simulator',
     license='MIT',
     python_requires='>=3.7',
